43adeb48478debb28ae1fff9667cc8ca7b4d3968
parham-box <parham.p@zoho.com>
2018-10-17 20:31:52 +0330

    Updated for Swift 4
    updated version of SwiftJalaali for Swift 4 and Ios 12

--- SwiftJalaali.swift
+++ SwiftJalaali.swift
@@ -27,7 +27,7 @@ public typealias GregorianDate = (year: GregorianYear, month: GregorianMonth, da
 
 // Jalaali years starting the 33-year rule.
 let breaks =  [ -61, 9, 38, 199, 426, 686, 756, 818, 1111, 1181, 1210
-  , 1635, 2060, 2097, 2192, 2262, 2324, 2394, 2456, 3178
+    , 1635, 2060, 2097, 2192, 2262, 2324, 2394, 2456, 3178
 ]
 
 /// This function determines if the Jalaali (Persian) year is
@@ -43,49 +43,49 @@ let breaks =  [ -61, 9, 38, 199, 426, 686, 756, 818, 1111, 1181, 1210
 /// @see: http://www.astro.uni.torun.pl/~kb/Papers/EMP/PersianC-EMP.htm
 /// @see: http://www.fourmilab.ch/documents/calendar/
 func jalCal(jy: JalaaliYear) -> (leapOffset: Int, year: GregorianYear, dayInMarch: Int) {
-  assert(jy >= breaks.first, "invalid jalaali year \(jy), should be >= \(breaks.first)")
-  assert(jy < breaks.last, "invalid jalaali year \(jy), should be < \(breaks.last)")
-  
-  let gy = jy + 621
-  var leapJ = -14
-  let jump = 0
-  
-  // Find the limiting years for the Jalaali year jy.
-  var jp = breaks[0]
-  for i in 1 ..< breaks.count {
-    let jm = breaks[i]
-    let jump = jm - jp
-    if jy < jm {
-      break
+    assert(jy >= breaks.first!, "invalid jalaali year \(jy), should be >= \(String(describing: breaks.first))")
+    assert(jy < breaks.last!, "invalid jalaali year \(jy), should be < \(String(describing: breaks.last))")
+    
+    let gy = jy + 621
+    var leapJ = -14
+    let jump = 0
+    
+    // Find the limiting years for the Jalaali year jy.
+    var jp = breaks[0]
+    for i in 1 ..< breaks.count {
+        let jm = breaks[i]
+        let jump = jm - jp
+        if jy < jm {
+            break
+        }
+        leapJ += jump / 33 * 8 + jump % 33 / 4
+        jp = jm
     }
-    leapJ += jump / 33 * 8 + jump % 33 / 4
-    jp = jm
-  }
-  var n = jy - jp
-  
-  // Find the number of leap years from AD 621 to the beginning
-  // of the current Jalaali year in the Persian calendar.
-  leapJ += n / 33 * 8 + (n % 33 + 3) / 4
-  if jump % 33 == 4 && jump - n == 4 {
-    leapJ += 1
-  }
-  
-  // And the same in the Gregorian calendar (until the year gy).
-  let leapG = gy / 4 - ((gy / 100) + 1) * 3 / 4 - 150
-  
-  // Determine the Gregorian date of Farvardin the 1st.
-  let march = 20 + leapJ - leapG
-  
-  // Find how many years have passed since the last leap year.
-  if jump - n < 6 {
-    n = n - jump + (jump + 4) / 33 * 33
-  }
-  var leap = ((n + 1) % 33 - 1) % 4
-  if leap == -1 {
-    leap = 4
-  }
-  
-  return (leap, gy, march)
+    var n = jy - jp
+    
+    // Find the number of leap years from AD 621 to the beginning
+    // of the current Jalaali year in the Persian calendar.
+    leapJ += n / 33 * 8 + (n % 33 + 3) / 4
+    if jump % 33 == 4 && jump - n == 4 {
+        leapJ += 1
+    }
+    
+    // And the same in the Gregorian calendar (until the year gy).
+    let leapG = gy / 4 - ((gy / 100) + 1) * 3 / 4 - 150
+    
+    // Determine the Gregorian date of Farvardin the 1st.
+    let march = 20 + leapJ - leapG
+    
+    // Find how many years have passed since the last leap year.
+    if jump - n < 6 {
+        n = n - jump + (jump + 4) / 33 * 33
+    }
+    var leap = ((n + 1) % 33 - 1) % 4
+    if leap == -1 {
+        leap = 4
+    }
+    
+    return (leap, gy, march)
 }
 
 /// Converts a date of the Jalaali calendar to the Julian Day number.
@@ -95,9 +95,9 @@ func jalCal(jy: JalaaliYear) -> (leapOffset: Int, year: GregorianYear, dayInMarc
 /// @param jd Jalaali day (1 to 29/31)
 /// @return Julian Day number
 func j2d(jy: JalaaliYear, _ jm: JalaaliMonth, _ jd: JalaaliDay) -> JulianDayNumber {
-  let r = jalCal(jy)
-  let t = g2d(r.year, 3, r.dayInMarch) + (jm - 1) * 31
-  return t - jm / 7 * (jm - 7) + jd - 1
+    let r = jalCal(jy: jy)
+    let t = g2d(gy: r.year, 3, r.dayInMarch) + (jm - 1) * 31
+    return t - jm / 7 * (jm - 7) + jd - 1
 }
 
 /// Converts the Julian Day number to a date in the Jalaali calendar.
@@ -108,35 +108,35 @@ func j2d(jy: JalaaliYear, _ jm: JalaaliMonth, _ jd: JalaaliDay) -> JulianDayNumb
 ///   jm: Jalaali month (1 to 12)
 ///   jd: Jalaali day (1 to 29/31)
 func d2j(jdn: JulianDayNumber) -> JalaaliDate {
-  let gy = d2g(jdn).year // Calculate Gregorian year (gy).
-  var jy = gy - 621
-  let r = jalCal(jy)
-  let jdn1f = g2d(gy, 3, r.dayInMarch)
-  var jd, jm, k: Int
-  
-  // Find number of days that passed since 1 Farvardin.
-  k = jdn - jdn1f
-  if k >= 0 {
-    if k <= 185 {
-      // The first 6 months.
-      jm = 1 + k / 31
-      jd = k % 31 + 1
-      return (jy, jm, jd)
+    let gy = d2g(jdn: jdn).year // Calculate Gregorian year (gy).
+    var jy = gy - 621
+    let r = jalCal(jy: jy)
+    let jdn1f = g2d(gy: gy, 3, r.dayInMarch)
+    var jd, jm, k: Int
+    
+    // Find number of days that passed since 1 Farvardin.
+    k = jdn - jdn1f
+    if k >= 0 {
+        if k <= 185 {
+            // The first 6 months.
+            jm = 1 + k / 31
+            jd = k % 31 + 1
+            return (jy, jm, jd)
+        } else {
+            // The remaining months.
+            k -= 186
+        }
     } else {
-      // The remaining months.
-      k -= 186
+        // Previous Jalaali year.
+        jy -= 1
+        k += 179
+        if r.leapOffset == 1 {
+            k += 1
+        }
     }
-  } else {
-    // Previous Jalaali year.
-    jy -= 1
-    k += 179
-    if r.leapOffset == 1 {
-      k += 1
-    }
-  }
-  jm = 7 + k / 30
-  jd = k % 30 + 1
-  return (jy, jm, jd)
+    jm = 7 + k / 30
+    jd = k % 30 + 1
+    return (jy, jm, jd)
 }
 
 /// Calculates the Julian Day number from Gregorian or Julian
@@ -150,11 +150,11 @@ func d2j(jdn: JulianDayNumber) -> JalaaliDate {
 /// @param gd Calendar day of the month (1 to 28/29/30/31)
 /// @return Julian Day number
 func g2d(gy: GregorianYear, _ gm: GregorianMonth, _ gd: GregorianDay) -> JulianDayNumber {
-  var d = (gy + (gm - 8) / 6 + 100100) * 1461 / 4
-    + (153 * ((gm + 9) % 12) + 2) / 5
-    + gd - 34840408
-  d = d - (gy + 100100 + (gm - 8) / 6) / 100 * 3 / 4 + 752
-  return d
+    var d = (gy + (gm - 8) / 6 + 100100) * 1461 / 4
+        + (153 * ((gm + 9) % 12) + 2) / 5
+        + gd - 34840408
+    d = d - (gy + 100100 + (gm - 8) / 6) / 100 * 3 / 4 + 752
+    return d
 }
 
 /// Calculates Gregorian and Julian calendar dates from the Julian Day number
@@ -167,41 +167,41 @@ func g2d(gy: GregorianYear, _ gm: GregorianMonth, _ gd: GregorianDay) -> JulianD
 ///   gm: Calendar month (1 to 12)
 ///   gd: Calendar day of the month M (1 to 28/29/30/31)
 func d2g(jdn: JulianDayNumber) -> GregorianDate {
-  var j = 4 * jdn + 139361631
-  j += (4 * jdn + 183187720) / 146097 * 3 / 4 * 4 - 3908
-  let i = j % 1461 / 4 * 5 + 308
-  let gd = i % 153 / 5 + 1
-  let gm = i / 153 % 12 + 1
-  let gy = j / 1461 - 100100 + (8 - gm) / 6
-  return (gy, gm, gd)
+    var j = 4 * jdn + 139361631
+    j += (4 * jdn + 183187720) / 146097 * 3 / 4 * 4 - 3908
+    let i = j % 1461 / 4 * 5 + 308
+    let gd = i % 153 / 5 + 1
+    let gm = i / 153 % 12 + 1
+    let gy = j / 1461 - 100100 + (8 - gm) / 6
+    return (gy, gm, gd)
 }
 
 /// Converts a Gregorian date to Jalaali.
 public func toJalaali(gy: GregorianYear, _ gm: GregorianMonth, _ gd: GregorianDay) -> JalaaliDate {
-  return d2j(g2d(gy, gm, gd))
+    return d2j(jdn: g2d(gy: gy, gm, gd))
 }
 
 /// Converts a Jalaali date to Gregorian.
 public func toGregorian(jy: JalaaliYear, _ jm: JalaaliMonth, _ jd: JalaaliDay) -> GregorianDate {
-  return d2g(j2d(jy, jm, jd))
+    return d2j(jdn: j2d(jy: jy, jm, jd))
 }
 
 /// Checks whether a Jalaali date is valid or not.
 public func isValidJalaaliDate(jy: JalaaliYear, _ jm: JalaaliMonth, _ jd: JalaaliDay) -> Bool {
-  return  jy >= breaks.first && jy < breaks.last &&
-    jm >= 1 && jm <= 12 &&
-    jd >= 1 && jd <= lastDayOfJalaaliMonth(jy, jm)
+    return  jy >= breaks.first! && jy < breaks.last! &&
+        jm >= 1 && jm <= 12 &&
+        jd >= 1 && jd <= lastDayOfJalaaliMonth(jy: jy, jm)
 }
 
 /// Is this a leap year or not?
 public func isLeapJalaaliYear(jy: JalaaliYear) -> Bool {
-  return jalCal(jy).leapOffset == 0
+    return jalCal(jy: jy).leapOffset == 0
 }
 
 /// Number of days in a given month in a Jalaali year.
 public func lastDayOfJalaaliMonth(jy: JalaaliYear, _ jm: JalaaliMonth) -> JalaaliDay {
-  if jm <= 6 { return 31 }
-  if jm <= 11 { return 30 }
-  if isLeapJalaaliYear(jy) { return 30 }
-  return 29
+    if jm <= 6 { return 31 }
+    if jm <= 11 { return 30 }
+    if isLeapJalaaliYear(jy: jy) { return 30 }
+    return 29
 }
